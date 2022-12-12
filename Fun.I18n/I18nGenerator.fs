5f9f7f4a9561b2a921f54d6ab748d47336c2c415
binwen <albertwoo_slaveoftime@hotmail.com>
2022-12-12 15:55:54 +0800

    Support generating fsharp code

--- Fun.I18n/I18nGenerator.fs
+++ Fun.I18n/I18nGenerator.fs
@@ -11,3 +11,12 @@ type I18nGenerator =
     /// <param name="targetNamespace">the namespace which all generated types should belong to</param>
     static member GenerateForCSharp(sourceFile: string, targetDir: string, targetNamespace: string) =
         CSharp.generateSourceCode sourceFile targetDir targetNamespace
+
+    /// <summary>
+    /// Generate csharp source code for mapping json string to related types
+    /// </summary>
+    /// <param name="sourceFile">source json file path</param>
+    /// <param name="targetDir">the directoy which used to save the generated csharp file</param>
+    /// <param name="targetNamespace">the namespace which all generated types should belong to</param>
+    static member GenerateForFSharp(sourceFile: string, targetDir: string, targetNamespace: string) =
+        FSharp.generateSourceCode sourceFile targetDir targetNamespace
